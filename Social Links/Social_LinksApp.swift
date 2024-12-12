473bc52c168932fe769b544abb5b6e2e73f59cd6
Ajmal <ajmal.poovanath@gmail.com>
2024-12-12 20:27:24 +0530

    Initial Commit

---
+++ Social Links/Social_LinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_LinksApp.swift
+//  Social Links
+//
+//  Created by Ajmal Poovanath on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Social_LinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Social LinksTests/Social_LinksTests.swift
@@ -0,0 +1,17 @@
+//
+//  Social_LinksTests.swift
+//  Social LinksTests
+//
+//  Created by Ajmal Poovanath on 12/12/24.
+//
+
+import Testing
+@testable import Social_Links
+
+struct Social_LinksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
