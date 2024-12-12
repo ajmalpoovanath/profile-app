b30e5f81f4da10a80f4d037d5c3916cf2be2112c
Ajmal <ajmal.poovanath@gmail.com>
2024-12-12 20:47:09 +0530

    Create layout

--- Social Links/ContentView.swift
+++ Social Links/ContentView.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            
+           //Background color
+            Color(red: 20 / 255, green: 20 / 255, blue: 20 / 255)
+                .ignoresSafeArea()
+            
+            ZStack{
+                //Card
+                Rectangle()
+                    .fill(Color(red: 31 / 255, green: 31 / 255, blue: 31 / 255))
+                    .cornerRadius(20)
+                    .shadow(color: Color.black.opacity(0.1), radius: 10, x: 0, y: 10)
+                    .frame(width: 350, height: 600)
+                
+                //Card content
+                ZStack{
+                    
+                }
+            }
+            
+            
+            
         }
-        .padding()
     }
 }
 
